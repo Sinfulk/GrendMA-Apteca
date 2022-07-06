b27f01d4dbf327ad5438a9077395bec9649aea4e
Ярослав Щеглов <yaroslav@MacBook-Pro-Aroslav.local>
2022-07-06 16:35:31 +0300

    start project

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
+const path = require("path");
+const mainRouter = require("./routes/mainRouter");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
 
 // подключаем hbs
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+
+// добавление локальной переменной во все хбс
+app.use((req, res, next) => {
+  res.locals.hello = "Hello from locals";
+  next();
+});
+
+app.use("/", mainRouter); // подключили роутер
 
 app.listen(PORT, () => {
   console.log(`server start PORT ${PORT}`);

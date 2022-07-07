ac34415c9d6dc11f90c777d2cb0ab2cbabb0e2b9
Aleksandr <sasha-pticin@inbox.ru>
2022-07-07 16:14:09 +0300

    cookie ok'

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const sessionConfig = {
   secret: process.env.SESSION_SECRET ?? "test", // Секретное слово для шифрования, может быть любым
   store: new FileStore(),
   resave: false, // Пересохранять ли куку при каждом запросе
-  saveUninitialized: true, // Создавать ли сессию без инициализации ключей в req.session
+  saveUninitialized: false, // Создавать ли сессию без инициализации ключей в req.session
   cookie: {
     maxAge: 1000 * 60 * 60 * 12, // Срок истечения годности куки в миллисекундах
     httpOnly: true, // Серверная установка и удаление куки, по умолчанию true

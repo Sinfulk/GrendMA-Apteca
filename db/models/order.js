84256a108fadca1ddb37f9b2100c9ec5e6bb5dfd
Shuzkeman <shuzkeman@mail.ru>
2022-07-07 15:06:15 +0300

    Order database created

--- db/models/order.js
+++ db/models/order.js
@@ -3,25 +3,22 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
+  class Order extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsToMany(User, { through: 'Order', foreignKey: 'product_id' });
-
       // define association here
     }
   }
-  Product.init({
-    product_name: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    picture: DataTypes.TEXT
+  Order.init({
+    user_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Product',
+    modelName: 'Order',
   });
-  return Product;
+  return Order;
 };

4f11f5b60f599a7c13e58ec42cc2a711e91d7192
Ваше Имя <you@example.com>
2022-07-07 11:32:09 +0300

    hbs

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const hbs = require("hbs");
 const path = require("path");
 const mainRouter = require("./routes/mainRouter");
 require("dotenv").config();
@@ -10,12 +9,34 @@ const PORT = process.env.PORT || 8080;
 // подключаем hbs
 app.set("view engine", "hbs");
 
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // добавление локальной переменной во все хбс
 app.use((req, res, next) => {
-  res.locals.hello = "Hello from locals";
+  res.locals.list = [
+    {
+      name: "парацетамол",
+      picture: "https://f.stolichki.ru/s/drugs/large/10/10189.jpg",
+      price: 500,
+    },
+    {
+      name: "парацетамол",
+      picture: "https://f.stolichki.ru/s/drugs/large/10/10189.jpg",
+      price: 500,
+    },
+    {
+      name: "парацетамол",
+      picture: "https://f.stolichki.ru/s/drugs/large/10/10189.jpg",
+      price: 500,
+    },
+  ];
+  res.locals.sum = res.locals.list?.reduce((acc, el) => {
+    acc + el.price
+  },0)
   next();
 });
-
 app.use("/", mainRouter); // подключили роутер
 
 app.listen(PORT, () => {
---
+++ routes/mainRouter.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get("/", (req, res) => {
+    res.render('basket')
+})
+
+module.exports = router

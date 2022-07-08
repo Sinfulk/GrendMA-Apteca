b6033b5b20dbc4d84b15dfdf76476242e2e33276
Ваше Имя <you@example.com>
2022-07-08 14:31:18 +0300

    position items ready

--- public/bascket.js
+++ public/bascket.js
@@ -1,19 +1,19 @@
-const main = document.querySelector(".main");
-main.addEventListener("click", async (e) => {
+const main = document.querySelector('.main');
+main.addEventListener('click', async (e) => {
   e.preventDefault();
-  if (e.target.name === "toBusket") {
+  if (e.target.name === 'toBusket') {
     const response = await fetch(
       `http://localhost:3000/product/${e.target.id}`
     );
     if (response.ok) {
       const data = await response.json();
-      console.log(">>>>>>", data);
+      console.log('>>>>>>', data);
       if (localStorage.busket) {
         localStorage.busket = JSON.stringify([
           ...JSON.parse(localStorage.busket),
           data,
         ]);
       } else localStorage.busket = JSON.stringify([data]);
-    } else console.log("Понял запрос нет продукта ");
+    } else console.log('Понял запрос нет продукта ');
   }
 });
--- public/front.js
+++ public/front.js
@@ -1,6 +1,6 @@
-console.log("i am end");
+console.log('i am end');
 getProducts = () => {
-  const productLocalStorage = localStorage.getItem("busket");
+  const productLocalStorage = localStorage.getItem('busket');
   if (productLocalStorage !== null) {
     return JSON.parse(productLocalStorage);
   }
@@ -9,21 +9,28 @@ getProducts = () => {
 
 const products = getProducts();
 console.log(products);
-const container = document.querySelector(".container1");
+const container = document.querySelector('.main');
 
 const HTMLs = products
-  .map((el) => {
-    return `
-    <div class="entries-list no-bullets no-padding" class="card" style="width: 18rem;">
-    <img src="${el.picture}" width="200px" class="card-img-top" alt="...">
-    <div class="card-body">
-     <h5 class="card-title">${el.product_name}</h5>
-     <p class="card-text">${el.price} руб.</p>
-     <a href="#" id="${el.id}" name ="toBusket" class="btn btn-primary "> Удалить </a>
+  .map(
+    (el) => `
+   <div class="main">
+    <div class="oneItem">
+      <div class="name">${el.product_name}</div>
+      <div class="divImg">
+        <img
+          class="img"
+          src=${el.picture}
+          alt=""
+        />
+      </div>
+      <div class="price">${el.price} ₽.</div>
+      <a href="#" id="${el.id}" name ="toBusket" class="btn btn-primary";">В корзину </a>
     </div>
-   </div>
-`;
-  })
-  .reduce((a, b) => a + b, "");
+  </div>
+`,
+  )
+  .reduce((a, b) => a + b, '');
 
-container.insertAdjacentHTML("afterbegin", HTMLs);
+container.insertAdjacentHTML('afterbegin', HTMLs);
+
